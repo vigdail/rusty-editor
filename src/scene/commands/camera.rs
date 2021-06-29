19fd732b6a6263eb6f6845d5bcfca6342d299846
Константин Бедрицкий <kmb2@tpu.ru>
2021-06-29 21:05:07 +0600

    SetCameraPreviewCommand::execute() refactoring

--- src/scene/commands/camera.rs
+++ src/scene/commands/camera.rs
@@ -19,19 +19,19 @@ define_node_command!(SetZFarCommand("Set Camera Z Far", f32) where fn swap(self,
 
 #[derive(Debug)]
 pub struct SetCameraPreviewCommand {
-    node: Handle<Node>,
+    handle: Handle<Node>,
     value: bool,
     old_value: bool,
-    last_active: Vec<Handle<Node>>,
+    prev_active: Vec<Handle<Node>>,
 }
 
 impl SetCameraPreviewCommand {
     pub fn new(node: Handle<Node>, value: bool) -> Self {
         Self {
-            node,
+            handle: node,
             value,
             old_value: false,
-            last_active: Vec::new(),
+            prev_active: Vec::new(),
         }
     }
 }
@@ -44,52 +44,49 @@ impl<'a> Command<'a> for SetCameraPreviewCommand {
     }
 
     fn execute(&mut self, context: &mut Self::Context) {
-        let camera = context.scene.graph[self.node].as_camera_mut();
+        let camera = context.scene.graph[self.handle].as_camera_mut();
         self.old_value = camera.is_enabled();
         camera.set_enabled(self.value);
 
         let editor_camera_handle = context.editor_scene.camera_controller.camera;
+        let editor_camera = context.scene.graph[editor_camera_handle].as_camera_mut();
+        editor_camera.set_enabled(!self.value);
 
-        self.last_active = context
-            .scene
-            .graph
-            .pair_iter_mut()
-            .filter_map(|(handle, node)| {
-                if self.node == handle || editor_camera_handle == handle {
-                    return None;
-                }
-
-                if let Node::Camera(cam) = node {
-                    if cam.is_enabled() {
-                        cam.set_enabled(false);
-                        Some(handle)
+        // disable other cameras and save their handles to be able to revert
+        if self.value {
+            self.prev_active = context
+                .scene
+                .graph
+                .pair_iter_mut()
+                .filter(|(handle, _)| handle != &self.handle && handle != &editor_camera_handle)
+                .filter_map(|(handle, node)| {
+                    if let Node::Camera(cam) = node {
+                        if cam.is_enabled() {
+                            cam.set_enabled(false);
+                            Some(handle)
+                        } else {
+                            None
+                        }
                     } else {
                         None
                     }
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>();
-
-        let editor_camera = context.scene.graph[editor_camera_handle].as_camera_mut();
-        editor_camera.set_enabled(!self.value);
+                })
+                .collect();
+        }
     }
 
     fn revert(&mut self, context: &mut Self::Context) {
-        for handle in self.last_active.iter_mut() {
+        for handle in self.prev_active.iter_mut() {
             let camera = context.scene.graph[*handle].as_camera_mut();
             camera.set_enabled(true);
         }
-        let camera = context.scene.graph[self.node].as_camera_mut();
+
+        let camera = context.scene.graph[self.handle].as_camera_mut();
         camera.set_enabled(self.old_value);
 
-        let editor_camera_node = context.editor_scene.camera_controller.camera;
-        let editor_camera = context.scene.graph[editor_camera_node].as_camera_mut();
-        if self.last_active.len() == 0 {
-            editor_camera.set_enabled(!self.old_value);
-        } else {
-            editor_camera.set_enabled(false);
-        }
+        let editor_camera_handle = context.editor_scene.camera_controller.camera;
+        let editor_camera = context.scene.graph[editor_camera_handle].as_camera_mut();
+        let editor_camera_enabled = !self.old_value && self.prev_active.is_empty();
+        editor_camera.set_enabled(editor_camera_enabled);
     }
 }
